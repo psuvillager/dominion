d01fd936bb3a0d7514d1abf355df1079251f105a
Cat <paisley@hotmail.com>
2019-12-29 17:32:14 -0500

    Added Node and app.js and played with app.js

---
+++ app.js
@@ -0,0 +1,65 @@
+const
+  http = require('http'),
+  serverModifications = {}, // Customize here
+  serverOptions = setServerOptions(serverModifications);
+startServer(serverOptions);
+
+
+// I need to customize how processRequest behaves based on a `responseOptions`
+//   object. This object needs to be a modifiable version of the 
+//   `defaultResponseOptions` property of the `serverOptions` object passed
+//   in to `startServer`.
+function processRequest(req, res){
+  
+  /*
+  res.statuscode = serverOptions.responseDefaultStatusCode;
+  if(typeof responseHeaders === "Object"){
+    Object.keys(getResponseHeaders(defaultHeaders, modifications)).forEach(key => {
+      res.setHeader(key, responseHeaders[key]);
+    });
+  }
+  res.end(getResponseBody(bodyOptions));
+  */
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.end("Hello World!");
+}
+
+
+// The functions below are boilerplate
+
+function setServerOptions(modifications){
+  const defaultServerOptions = {
+    port: 3000,
+    hostname: "127.0.0.1", // Change this for custom hosts
+    callback: (port, hostname = "") => console.log("Server running " +
+      hostname ? `at http://${hostname}:${port}` : `on port ${port}`
+    ),
+    defaultResponseOptions: {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "text/plain",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: "Hello World!",
+    }
+  };
+  const modifiedServerOptions = {};
+  modifications = modifications || {};
+  Object.assign(modifiedServerOptions, defaultServerOptions, modifications);
+  return modifiedServerOptions;
+}
+
+function startServer(opts){
+  const server = http.createServer(opts.requestHandler);
+  if(opts.hostname){
+    console.log("got hostname");
+    if(opts.callback){
+      console.log("got callback");
+      server.listen(opts.port, opts.hostname, opts.callback(opts.port, opts.hostname));
+    }
+    else server.listen(opts.port, opts.hostname);
+  }
+  else server.listen(opts.port);
+}
